87c97595382387b13d75d37d1d23c04b60a0b029
NipaDelara <nipadelara@hotmail.com>
2025-08-22 19:15:39 +0300

    Tein tehtävä 6

---
+++ Tehtävä6.py
@@ -0,0 +1,11 @@
+import random
+
+kood3 = (str(random.randint(0,9)) + str(random.randint(0,9))
+         + str(random.randint(0,9)))
+
+kood4 = (str(random.randint(1,6)) + str(random.randint(1,6))
+         + str(random.randint(1,6)) + str(random.randint(1,6)))
+
+
+print("kolmenumeroisen koodin:", kood3)
+print("Nelinumeroisen koodin:", kood4)
